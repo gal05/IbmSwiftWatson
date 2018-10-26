050520fba7f16e4df647cb21bf56ad80a2b31f0d
Guerra Jose <jose.guerra.g@tecsup.edu.pe>
2018-10-26 15:09:23 -0500

    done ahora para instalar sdk de waatson

--- IbmSwiftWatson/ViewController.swift
+++ IbmSwiftWatson/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var textStatusLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func checkButtonPressed(_ sender: Any) {
+        NSLog(textField.text!)
+        textStatusLabel.text=textField.text
+    }
+    
 }
 
